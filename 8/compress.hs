a568b947121bcfc52ce153d5e6e4e02d79f7028d
Tiddo Langerak <tiddolangerak@gmail.com>
2015-05-22 13:38:39 +0200

    Problem 8 improvements

--- 8/compress.hs
+++ 8/compress.hs
@@ -1,7 +1,14 @@
-compress :: (Eq a) => [a] -> [a]
+import Data.List
+
+-- Initial attempt
+compress :: Eq a => [a] -> [a]
 compress [] = []
 compress [x] = [x]
 compress (x:y:xs) =
     if x == y
       then compress (x:xs)
       else x:(compress (y:xs))
+
+-- After learning about group
+compress' :: Eq a => [a] -> [a]
+compress' = map head . group
