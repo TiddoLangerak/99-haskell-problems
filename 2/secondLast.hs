5ec25e2c54e0425387e4cfe9e3ab04018d350b3e
Tiddo Langerak <tiddolangerak@gmail.com>
2015-05-22 12:48:38 +0200

    Question 2 better solution

--- 2/secondLast.hs
+++ 2/secondLast.hs
@@ -1,5 +1,12 @@
+--Initial attempt
 secondLast :: [a] -> a
 secondLast [] = error "Cannot get second last from empty list"
 secondLast [x] = error "Cannot get second last from list with just one item"
 secondLast (x:_:[]) = x
 secondLast (_:x:xs) = secondLast (x:xs)
+
+--This is how it's supposed to be done
+secondLast' :: [a] -> a
+secondLast [] = error "Cannot get second last from empty list"
+secondLast [x] = error "Cannot get second last from list with just one item"
+secondLast' = last . init
