679a7bf9962e66e05da57d96badf0d487031efeb
Tiddo Langerak <tiddolangerak@gmail.com>
2015-05-27 14:36:37 +0200

    Moar solutions for problem 14

--- 14/duplicate.hs
+++ 14/duplicate.hs
@@ -4,3 +4,7 @@ duplicate (x:xs) = [x, x] ++ (duplicate xs)
 
 duplicate' :: [a] -> [a]
 duplicate' = concatMap (\x -> [x, x])
+
+--Same, but with monads:
+duplicate'' :: [a] -> [a]
+duplicate'' xs = xs >>= (\x -> [x, x])
