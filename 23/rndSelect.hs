7ce5bc8b9279c4106af3966e1357aaf641d961f3
Tiddo Langerak <tiddolangerak@gmail.com>
2015-06-12 13:41:35 +0200

    Alternate version of problem 23
    note that it does seem to get stuck in an error state when it has errored. not sure why

--- 23/rndSelect.hs
+++ 23/rndSelect.hs
@@ -10,8 +10,20 @@ rndSelect xs c = randomRIO (0, length xs - 1)
            >>= return . (:) element
   )
 
+-- After learning that the RandomGen versions can be implemented without monads
+rndSelect' :: RandomGen g => [a] -> Int -> g -> ([a], g)
+rndSelect' _ 0 gen = ([], gen)
+rndSelect' [] _ _ = error "Number of items requested is larger than list"
+rndSelect' xs n gen = ((xs !! i) : rest, gen'')
+                    where (i, gen') = randomR (0, length xs - 1) gen
+                          (rest, gen'') = (rndSelect' (removeAt xs i) (n - 1) gen')
+
+rndSelectIO' :: [a] -> Int -> IO [a]
+rndSelectIO' xs n = getStdRandom $ rndSelect' xs n
+
 removeAt :: [a] -> Int -> [a]
 removeAt xs n
   | length xs <= n || n < 0 = error "Index out of bounds"
   | otherwise = let (ys, zs) = splitAt n xs
                     in ys ++ (tail zs)
+
